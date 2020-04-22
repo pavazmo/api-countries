9c57f4544bc6393395be3b38ccf53bf063f9da70
pavazmo <pavazmo@gmail.com>
2020-04-22 20:11:59 +0200

    First commit and creating script to load GeoJSON

---
+++ README.md
@@ -0,0 +1,13 @@
+# online_payment
+
+This is a code repository for a test online payments.
+
+## Setup
+
+* Install dependencies. Just run
+<code>npm install</code>
+from the root of the directory structure.
+* Run the script for loading geoJSON in database
+<code>node convert-to-geojson.js</code>
+* Run the app
+<code>node app</code>
---
+++ convert-to-geojson.js
@@ -0,0 +1,26 @@
+const GeoJSON = require('geojson');
+const MongoClient = require('mongodb').MongoClient;
+const neatCsv = require('neat-csv');
+const fs = require('fs')
+const url = 'mongodb://localhost:27017/';
+
+fs.readFile('./countries.csv', async (err, data) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  
+  var data = await neatCsv(data);
+  var jsonData = GeoJSON.parse(data, {Point: ['latitude', 'longitude']})
+  var countries = jsonData.features;
+
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db('guruwalk-test');
+    dbo.collection('countries').insert(countries, function(err, res) {
+        if (err) throw err;
+        console.log('Countries inserted');
+        db.close();
+    });
+  });
+})
